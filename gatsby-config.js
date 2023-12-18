c69e02fcbb5867f60bfb734b8bd15e986dd76a1c
Yaodong Zhao <git@yaodong.org>
2023-12-17 22:10:02 -0800

    Support Github Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: "夜间日志",
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
